ae45c68436131ded4afb088796017ce2e0aa5ae6
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-08-05 07:35:38 +0900

    feat: restore app launcher request handler

--- lib/requestHandler.ts
+++ lib/requestHandler.ts
@@ -12,7 +12,11 @@ import {
 } from '../services/brightness';
 import { getPowerProfile, setPowerProfile } from '../services/powerProfile';
 import { isRecording, startRecord, stopRecord } from '../services/recordService';
-import { toggleControlCenter, toggleDateWeather } from '../services/windowManager';
+import {
+  toggleAppLauncher,
+  toggleControlCenter,
+  toggleDateWeather,
+} from '../services/windowManager';
 import { compileAndReloadCss } from './css';
 
 type ResponseCallback = (response: string) => void;
@@ -104,6 +108,11 @@ export function requestHandler(request: string[], res: ResponseCallback) {
     case 'toggle-cc':
       toggleControlCenter();
       res('Toggled Control Center');
+      break;
+
+    case 'toggle-launcher':
+      toggleAppLauncher();
+      res('Toggled App Launcher');
       break;
 
     case 'list-windows': {
