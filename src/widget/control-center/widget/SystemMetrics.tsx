c0970ad97af01fda2ccb5f7be6005a195008e553
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-08-15 12:18:10 +0900

    refactor: simplify control center system metrics

--- src/widget/control-center/widget/SystemMetrics.tsx
+++ src/widget/control-center/widget/SystemMetrics.tsx
@@ -1,6 +1,7 @@
 import { Gtk } from 'ags/gtk4';
 
-import { RamData, cpuUsage, gpuUsage, openSystemMonitor, ramUsage } from '../../../stores/system';
+import { cpuUsage, gpuUsage, openSystemMonitor, ramUsage } from '../../../stores/system';
+import type { RamData } from '../../../stores/system';
 import CircularProgress from '../../common/CircularProgress';
 
 export default function SystemMetrics() {
@@ -9,7 +10,7 @@ export default function SystemMetrics() {
       <box halign={Gtk.Align.CENTER}>
         <button
           css="background: transparent; border: none; box-shadow: none; padding: 0; border-radius: 50%;"
-          onClicked={() => openSystemMonitor()}
+          onClicked={openSystemMonitor}
         >
           <CircularProgress
             variable={cpuUsage}
@@ -25,7 +26,7 @@ export default function SystemMetrics() {
       <box halign={Gtk.Align.CENTER}>
         <button
           css="background: transparent; border: none; box-shadow: none; padding: 0; border-radius: 50%;"
-          onClicked={() => openSystemMonitor()}
+          onClicked={openSystemMonitor}
         >
           <CircularProgress
             variable={ramUsage}
@@ -41,7 +42,7 @@ export default function SystemMetrics() {
       <box halign={Gtk.Align.CENTER}>
         <button
           css="background: transparent; border: none; box-shadow: none; padding: 0; border-radius: 50%;"
-          onClicked={() => openSystemMonitor()}
+          onClicked={openSystemMonitor}
         >
           <CircularProgress
             variable={gpuUsage}
